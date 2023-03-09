05c2c869a1d1193ad92a5b3422ada4c7c5ea05a0
Ksenia Tsutsalevich <ksyutsutsalevich@mail.ru>
2023-03-10 00:46:21 +0300

    Add ability to clear and make complete

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -6,6 +6,7 @@ type Props = {};
 type Task = {
     id: string;
     label: string;
+    isComplete: boolean;
 }
 
 const ListScreen: React.FC<Props> = () => {
@@ -16,21 +17,40 @@ const ListScreen: React.FC<Props> = () => {
     
     const handleNewTaskLabelKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
      if (e.key === 'Enter' && newTaskLabel !== '') {
-        setTasks((tasks) => [...tasks,{id: nanoid(), label: newTaskLabel}]);
+        setTasks((tasks) => [...tasks,{id: nanoid(), label: newTaskLabel, isComplete: false}]);
         setNewTaskLabel('');
         }
     };
 
+    const handleCompleteChange = (handledTask: Task) => (e: ChangeEvent<HTMLInputElement>) => {
+        setTasks(tasks => 
+            tasks.map(task => {
+                if (task.id === handledTask.id) 
+                    return {...task, isComplete: e.target.checked}
+                return task;
+            })
+        ); 
+    };
+
+    const handleClearClick = () => 
+        setTasks((tasks) => tasks.filter(task => !task.isComplete))
+
     return ( 
     <div>
-        <ul>
+        <div>
             {tasks.map((task: Task) => 
-                <li key={task.id}>{task.label}</li>)}
-        </ul>
+                <div key={task.id}>
+                    <input type="checkbox" checked={task.isComplete} onChange={handleCompleteChange(task)}/> {task.label}
+                </div>)}
+        </div>
         <input 
             value={newTaskLabel} 
             onChange={handleNewTaskLabelChange} 
-            onKeyDown={handleNewTaskLabelKeyDown}/> 
+            onKeyDown={handleNewTaskLabelKeyDown}
+        /> 
+        <div>
+        <button onClick={handleClearClick}>clear completed</button>
+        </div>        
     </div>
     );
     
